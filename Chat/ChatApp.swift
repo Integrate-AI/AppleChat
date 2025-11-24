f4dc148e20f69c171b0b3654dfdd197a3a38b99e
Eric English <eric.english@gmail.com>
2025-11-24 11:36:38 -0500

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Eric English on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatTests/ChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTests.swift
+//  ChatTests
+//
+//  Created by Eric English on 11/24/25.
+//
+
+import Testing
+@testable import Chat
+
+struct ChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
